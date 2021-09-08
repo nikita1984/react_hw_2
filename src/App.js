22ebeb4db692480679cfc01aac0a0b88184a20fa
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2021-09-08 23:16:10 +0300

    create robot answer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import MessageListComponent  from "./MessageListComponent.js";
 import InputMessageComponent from "./InputMessageComponent";
 
 function App() {
   const [inputMessage, setInputMessage] = useState({});
-  const [messagesArray, setMessagesArray] = useState([]);
+  const [userMessagesArray, setUserMessagesArray] = useState([]);
+  const [globalMessagesArray, setGlobalMessagesArray] = useState([]);
 
   const onSendMessage = () => {
-    setMessagesArray((prev) => [...prev, inputMessage]);
+    setUserMessagesArray((prev) => [...prev, inputMessage]);
+    setGlobalMessagesArray((prev) => [...prev, inputMessage]);
     setInputMessage({text: "", autor: ""});
   };
 
-  const renderMessageList = messagesArray.map((message, i) => (
+  const renderMessageList = globalMessagesArray.map((message, i) => (
       <div key={i}>
         <div>{message.autor}</div>
         <div>{message.text}</div>
       </div>
   ))
-  
+
+  const robotAnswer = () => {
+    setGlobalMessagesArray((prev) => [...prev, {text: "Надеюсь Вас устроит мой ответ", autor: "robot:"}]);    
+  };
+
+  useEffect(() => {
+    if(userMessagesArray.length !== 0){
+        robotAnswer();
+    }    
+  },[userMessagesArray]);
 
   return (
     <div className="mainWrapper">
